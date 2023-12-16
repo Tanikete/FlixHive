c117252b2bab08fe85732d3b56d00f18d8b9a44e
Guilbert21 <guilbertalexis534@gmail.com>
2023-12-16 21:21:15 +0400

    Add styled-components and login button to header component

--- my_app/src/components/header.jsx
+++ my_app/src/components/header.jsx
@@ -1,7 +1,18 @@
 import React from 'react'
-
+import styled from "styled-components";
+import {useNavigate} from "react-router-dom";
+import logo from "../assets/logo.png";
 export default function header() {
   return (
-    <div>header</div>
-  )
+    <Container>
+        <div className="logo">
+            <img src="{logo}" alt="logo" />
+        </div>
+        <button onClick={() => }>{props.login ? 'Log In' : 'Sign In'}</button>
+    </Container>
+  );
 }
+
+const Container = styled.div `
+
+`;
