7e170b9930775abbbfe60edf99266df042e51ad1
Yu <awushu6@gmail.com>
2023-12-19 22:06:08 +0400

    Update server.js file

--- server/server.js
+++ server/server.js
@@ -3,18 +3,14 @@ const cors = require('cors');
 const mongoose = require('mongoose');
 const userRoutes = require('./routes/UserRoutes');
 const app = express();
-require('dotenv').config(); // Load environment variables from .env file
-
 
 app.use(cors());
 app.use(express.json());
 
-console.log('MONGODB_URL:', process.env.MONGODB_URL);
-mongoose.connect(process.env.MONGODB_URL)
+mongoose.connect('mongodb://localhost:27017/FlixHive')
   .then(() => console.log('MongoDB Connected'))
-  .catch(err => console.error('MongoDB Connection Error:', err));
+  .catch(err => console.log(err));
 
-
-app.use("/", userRoutes);
+app.use("/api/user", userRoutes);
 
 app.listen(5000, () => console.log('Server Running'));
