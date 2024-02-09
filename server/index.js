c3979fc873e5ae6241b824870b0dc1aef21fdfed
Yu <awushu6@gmail.com>
2024-02-09 14:33:59 +0400

    Update MongoDB connection string

--- server/index.js
+++ server/index.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect('mongodb://localhost:27017/FlixHive')
+mongoose.connect('mongodb+srv://awushu6:tUekkXW7flLgZVbI@cluster0.tmyb1dy.mongodb.net/?retryWrites=true&w=majority')
   .then(() => console.log('MongoDB Connected'))
   .catch(err => console.log(err));
 
