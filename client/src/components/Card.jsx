4eb87d0616fadc8e114342813937bdaaf4ef3b07
Yu <awushu6@gmail.com>
2024-02-09 14:41:56 +0400

    Update API endpoints to use production URL

--- client/src/components/Card.jsx
+++ client/src/components/Card.jsx
@@ -27,7 +27,7 @@ export default React.memo(function Card({ index, movieData, isLiked = false }) {
 
   const addToList = async () => {
     try {
-      await axios.post("http://localhost:5000/api/user/add", {
+      await axios.post("https://flix-hive-backend.vercel.app/api/user/add", {
         email,
         data: movieData,
       });
